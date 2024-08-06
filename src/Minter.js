9b117cdcf60849a079c7b5f20db6b04453efa345
JackAutieri <jackautieri@gmail.com>
2024-08-05 20:36:41 -0400

    add all files to github

---
+++ src/Minter.js
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react";
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  mintNFT, // Add mintNFT here
+} from "./utils/interact.js";
+
+const Minter = (props) => {
+  // State variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+
+  useEffect(async () => { // TODO: implement
+    const { address, status } = await getCurrentWalletConnected();
+    setWallet(address);
+    setStatus(status);
+
+    addWalletListener();
+  }, []);
+
+  const connectWalletPressed = async () => { // TODO: implement
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  const onMintPressed = async () => { // TODO: implement
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
+  };
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("No fields can be left blank.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to MetaMask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊 <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install MetaMask, a virtual Ethereum wallet, in your browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title"> NFT Minter 1.0 </h1>
+      <p>
+        
+      </p>
+      <form>
+        <h2> Link to asset: </h2>
+        <input
+          type="text"
+          placeholder="e.g. https://gateway.pinata.cloud/ipfs/<hash>"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2> Give your NFT a Name: </h2>
+        <input
+          type="text"
+          placeholder="e.g. My first NFT!"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <h2> Description: </h2>
+        <input
+          type="text"
+          placeholder="e.g. CyberPunk-y"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+        Mint NFT
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
